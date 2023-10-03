237fac06f1b3263c987d0cd3339c587ac60b765e
Bruno Milano <brn.milano@hotmail.com>
2023-10-03 20:12:57 -0300

    Project -> QR Code Generator

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input")
+const qrCodeImg = document.querySelector("#qr-code img");
+
+function generateQRCode() {
+  const qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) {
+    return
+  };
+
+  qrCodeBtn.innerHTML = "Gerando código...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+
+    qrCodeBtn.innerHTML = "Código criado.";
+  });
+};
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQRCode();
+});
+
+qrCodeInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    generateQRCode();
+  }
+});
+
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+
+    qrCodeBtn.innerHTML = "Gerar QR Code";
+  }
+});
